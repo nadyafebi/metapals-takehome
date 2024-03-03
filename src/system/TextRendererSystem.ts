ddaafeeb80d6982875bdb32a781f1d8ccd0c4c60
Nadya Djojosantoso <hello@nadyafebi.com>
2024-03-03 23:01:25 +0700

    Add cache to TextRendererSystem

--- src/system/TextRendererSystem.ts
+++ src/system/TextRendererSystem.ts
@@ -2,11 +2,16 @@ import { Entity, System } from "../engine";
 import { TextComponent } from "../components/TextComponent";
 
 export class TextRendererSystem extends System {
+  renderCache: Record<string, string> = {};
+
   update(entities: Entity[]): void {
     for (const entity of entities) {
       const textComponent = entity.getComponent(TextComponent);
-      if (textComponent) {
+
+      // Update DOM only when the text changed.
+      if (textComponent && this.renderCache[entity.id] != textComponent.text) {
         entity.element.innerText = textComponent.text;
+        this.renderCache[entity.id] = textComponent.text;
       }
     }
   }
